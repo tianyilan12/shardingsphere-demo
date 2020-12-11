2cf481d232c304e114fc7432eed88fffc6f5a818
tianyilan12 <66726288+tianyilan12@users.noreply.github.com>
2020-12-11 11:08:02 +0800

    Update DDL.sql

--- demo-sharding/src/main/resources/sql/DDL.sql
+++ demo-sharding/src/main/resources/sql/DDL.sql
@@ -40,3 +40,16 @@ CREATE TABLE `user` (
   `user_id` bigint(20) NOT NULL,
   `user_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for encrypt_user
+-- ----------------------------
+DROP TABLE IF EXISTS `encrypt_user`;
+CREATE TABLE `encrypt_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) DEFAULT NULL,
+  `user_name_plain` varchar(200) DEFAULT NULL,
+  `pwd` varchar(200) DEFAULT NULL,
+  `assisted_query_pwd` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
